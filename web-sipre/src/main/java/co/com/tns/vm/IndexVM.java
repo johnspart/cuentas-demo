d3dc1f3bf9ecfc127038d679b2eea1c8209bd597
John López <best.spart@gmail.com>
2016-07-14 18:55:54 -0500

    commit

--- web-sipre/src/main/java/co/com/tns/vm/IndexVM.java
+++ web-sipre/src/main/java/co/com/tns/vm/IndexVM.java
@@ -1,13 +1,9 @@
 package co.com.tns.vm;
 
-<<<<<<< HEAD
 import co.com.tns.bs.cuenta.Cliente;
 
 public class IndexVM {
 	private Cliente cliente;
 	
-=======
-public class IndexVM {
-	private U
->>>>>>> master
+
 }
