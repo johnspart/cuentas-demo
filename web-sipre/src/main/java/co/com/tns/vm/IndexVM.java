c64ff959e28db22d2a17f8a8ea4c97d4b0f763fe
John López <best.spart@gmail.com>
2016-07-14 21:04:21 -0500

    Se agrega atdd

--- web-sipre/src/main/java/co/com/tns/vm/IndexVM.java
+++ web-sipre/src/main/java/co/com/tns/vm/IndexVM.java
@@ -15,8 +15,8 @@ public class IndexVM {
 
 	@Init
 	public void init(@ExecutionArgParam("cliente") Cliente cliente) {
-		this.cliente = new Cliente();
-		this.cuenta = new Cuenta();
+		this.cliente = cliente;
+		this.cuenta = cliente.getCuenta();
 		this.transFerenciaExitosa = false;
 	}
 
--- web-sipre/src/main/java/co/com/tns/vm/LoginVM.java
+++ web-sipre/src/main/java/co/com/tns/vm/LoginVM.java
@@ -2,10 +2,22 @@ package co.com.tns.vm;
 
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.zk.ui.Executions;
+
+import co.com.tns.bs.cuenta.Cliente;
+import co.com.tns.bs.cuenta.Cuenta;
 
 public class LoginVM {
 	@Command("login")
-	public void login(@BindingParam("user") String usuarios, @BindingParam("pass") String pass) {
-		System.out.println("Bien");
+	public void login(@BindingParam("user") String user, @BindingParam("pass") String pass) {
+		if (user != pass)
+			return;
+		Cliente cliente = new Cliente();
+		Cuenta cuenta = new Cuenta();
+		cuenta.setSaldo(1000);
+		cuenta.setFecha("2015-12-5");
+		cliente.setCuenta(cuenta);
+		Executions.getCurrent().setAttribute("cliente", cliente);
+		Executions.sendRedirect("/index.zul");
 	}
 }
